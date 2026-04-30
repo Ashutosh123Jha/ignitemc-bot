829cba7917d3e549f4d2bfce94988103641bd994
Ashutosh123Jha <115517203+Ashutosh123Jha@users.noreply.github.com>
2026-04-30 23:52:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -50,3 +50,13 @@ client.on("messageCreate", async (message) => {
 });
 
 client.login(process.env.TOKEN);
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is running");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Web server started");
+});
